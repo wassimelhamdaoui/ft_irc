658b2ee462207e72c2ae8b8c2727141f998f4777
Azedine ouhadou <aouhadou@student.1337.ma>
2023-04-03 14:57:33 +0000

    remove commented pass function command

--- main.cpp
+++ main.cpp
@@ -1,32 +1,5 @@
 #include "server.hpp"
 
-// void check_pass(char **tokens, int fd)
-// {
-//     std::string msg;
-//     server obj;
-//     int i = 0;
-//     while (tokens[i])
-//         i++;
-//     if (i != 2 || tokens[1] == NULL){
-//         msg = "invalid password arguments: try PASS <server password>\n";
-//         send(fd, msg.c_str(), msg.size(), 0);
-//         return;
-//     }
-//     std::string pass(tokens[1]);
-//     if (pass == obj.get_pass())
-//     {
-//         obj.set_authentication(true);
-//         msg = "Successful authentication...\n";
-//         send(fd, msg.c_str(), msg.size(), 0);
-//     }
-//     else
-//     {
-//         msg = "Invalid Password!\n";
-//         send(fd, msg.c_str(), msg.size(), 0);
-//     }
-    
-// }
-
 void    server::parse_request(char *read, int fd)
 {
     Client client(fd);
