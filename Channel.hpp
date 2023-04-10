ad05dd6163e27fea3b3dc6218ebbe4f4c2482e16
Azedine ouhadou <aouhadou@student.1337.ma>
2023-04-10 01:53:56 +0000

    finish part

--- Channel.hpp
+++ Channel.hpp
@@ -36,6 +36,7 @@ class Channel
 		void set_pass(std::string pass);
 		void set_topic(std::string topic);
 		void set_invite_only(bool invite_only);
+		void set_is_private(bool is_private);
 
 		//methods
 		void add_member(int fd);
