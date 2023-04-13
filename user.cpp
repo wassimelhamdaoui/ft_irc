fd9f8f7ab9468ac87bc6685bcdfca93a52cefd7f
Azedine ouhadou <aouhadou@student.1337.ma>
2023-04-13 22:40:32 +0000

    fix user command

--- user.cpp
+++ user.cpp
@@ -17,10 +17,13 @@ std::string server::user_response(std::vector<std::string> split, Client &client
 {
 	if(client.get_pass())
 	{
+		std::string nick = client.get_nick();
+		if (nick.empty())
+			nick = "*";
 		if(client.get_auth())
-			return(":localhost 462 " + client.get_nick() + " USER :You may not reregister\r\n");
+			return(":localhost 462 " + nick + " USER :You may not reregister\r\n");
 		if(split.size() < 5) // || split[1].length() != USERLEN
-			return(":localhost 461 " + client.get_nick() + " USER :Not enough parameters\r\n");
+			return(":localhost 461 " + nick + " USER :Not enough parameters\r\n");
 		client.set_userName(split[1]);
 		client.set_auth(true);
 		if(client.get_reg() && !client.get_print())
